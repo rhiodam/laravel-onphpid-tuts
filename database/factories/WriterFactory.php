96230c77953187d8961437cb418b39a548d06ec9
Rhioda muthie <rhiodam@gmail.com>
2019-06-28 08:55:27 +0700

    resign

---
+++ database/factories/WriterFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Writer;
+use Faker\Generator as Faker;
+
+$factory->define(Writer::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
